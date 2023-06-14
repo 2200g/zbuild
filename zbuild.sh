b2027f87f45ce14bc996a50f5693914240ae2652
aadi <aadi@aadi.net.in>
2023-06-14 13:30:31 +0530

    :uh:

--- zbuild.sh
+++ zbuild.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
 
 ## mkdir config
-mkdir ~/.config/zbuild.sh/ > /dev/null 2>&1
-touch ~/.config/zbuild.sh
+mkdir ~/.config/zbuild/ > /dev/null 2>&1
+touch ~/.config/zbuild/config
+
+config=~/.config/zbuild/config
+
+ducky=$(cat $config)
+
+abby=
+ziva= 
+
+zbuildinit() {
+	rm ~/.config/zbuild/zbuild.sh > /dev/null 2>&1
+	touch ~/.config/zbuild/config
+	read -r -p "zola dir [/home/aadi/site]: " dinozzo
+	read -r -p "site dir [/home/aadi/public]: " gibbs
+		
+	echo "zola = $dinozzo" >> $config
+	echo "site = $gibbs" >> $config
+}
+
+zbuild() {
+	echo 124
+	read -r -p "git commit: " mcgee
+	zola build
+	
+	cp -r -f $abby/public/* $ziva/
+	cd $ziva/ 
+	git add . && git commit -m "$mcgee"
+	git push
+}
+
+halp() {
+	echo hi
+}
 
 
-read -r -p "zola directory [/home/aadi/site]: " dinozzo
-read -r -p "site dir [/home/aadi/public]: " gibbs
-read -r -p "git commit: " mcgee
-
-
-## generates zola files
-zola build
-
-## copies stuff
-cp -r -f $dinozzo/public/* $gibbs/
-
-## git update
-cd $gibbs/ 
-git add . && git commit -m "$mcgee"
-git push
+while getopts ":hi" option; do
+	case $option in
+		h)
+			halp
+			exit;;
+		i)
+			zbuildinit;;
+	esac
+done
